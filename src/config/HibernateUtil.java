63ac1d704b1cf6a38dbb11473f82f22f26aa86f4
ffinkler <ffinkler@10.3.9.15>
2016-08-10 22:23:16 -0300

    alterado nome do arquivo HibernateUtil

--- src/config/HibernateUtil.java
+++ src/config/HibernateUtil.java
@@ -14,7 +14,7 @@ import org.hibernate.SessionFactory;
  *
  * @author asimon
  */
-public class NovoHibernateUtil {
+public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
     
